841c3278eb383047a56f230f18a576510406200f
apaytuvigallart <aleixpaytuvi@gmail.com>
2025-07-30 15:57:00 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from twilio.rest import Client
+
+from ai import generate_text
+from settings import ACCOUNT_SID, AUTH_TOKEN
+
+
+def main(receiver: int, sender: int) -> str:
+    """
+    Main function to send a notification using Twilio.
+    """
+    if ACCOUNT_SID is None or AUTH_TOKEN is None:
+        raise ValueError("Twilio credentials are not set, dropping ")
+
+    sentence = generate_text()
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    client.messages.create(to=f"+{receiver}", from_=f"+{sender}", body=sentence)
+
+    return "Message sent successfully: " + sentence
+
+
+main(
+    34123456789, 11234567890
+)  # Example phone numbers, include country code, i.e. +34 for Spain
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+ACCOUNT_SID = os.getenv("ACCOUNT_SID", None)
+AUTH_TOKEN = os.getenv("AUTH_TOKEN", None)
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", None)
