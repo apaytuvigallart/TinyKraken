9b04ec4aa6f0d8e180a74f08562151d50dd21481
apaytuvigallart <aleixpaytuvi@gmail.com>
2025-08-07 08:38:24 +0200

    fix: remove attributes to avoid getting error 'all attributes must be indexed'

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -7,13 +7,5 @@ resource "aws_dynamodb_table" "tiny_kraken_db" {
     name = "id"
     type = "S"
   }
-  attribute {
-    name = "text"
-    type = "S"
-  }
-  attribute {
-    name = "created_at"
-    type = "S"
-  }
   tags = var.common_tags
 }
